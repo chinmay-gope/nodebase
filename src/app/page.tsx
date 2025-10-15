2cf51d54a4cb3e1ded69086b77210a47aa6ef966
chinmay-gope <chinmaygope69@gmail.com>
2025-10-15 23:55:56 +0530

    authtication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import { caller } from "~/trpc/server";
-
+import { LogoutBtn } from "./logout";
+import { requireAuth } from "~/lib/auth.utils";
 export default async function Home() {
-  const users = await caller.getUsers()
+  await requireAuth();
+  const users = await caller.getUsers();
   return (
-    <div className="min-h-screen min-w-screen flex items-center justify-center">
-      {JSON.stringify(users, null, 2)}
+    <div className="min-h-screen min-w-screen flex items-center justify-center flex-col gap-y-5">
+      protected page
+      <div>{JSON.stringify(users, null, 2)}</div>
+      <LogoutBtn />
     </div>
   );
 }
